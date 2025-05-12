52e9627d4861344283e196616995d37b99a7be70
Nikos Fotiou <nikosft@gmail.com>
2025-05-12 12:48:16 +0300

    bug with Rekor fixed

--- Staas/Models/Sigstore/Rekor/v1/LogEntry.cs
+++ Staas/Models/Sigstore/Rekor/v1/LogEntry.cs
@@ -1,34 +1,50 @@
-﻿namespace Excid.Sigstore.Rekor.v1.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Excid.Sigstore.Rekor.v1.Models
 {
 
 	public class LogEntry
 	{
-		public string LogID { get; set; } = string.Empty;
-		public int LogIndex { get; set; }
-		public string Body { get; set; } = string.Empty;
-		public int IntegratedTime { get; set; }
-		public Attestation? Attestation { get; set; } = null;
-		public Verification? Verification { get; set; } = null;	
+        [JsonPropertyName("logID")]
+        public string LogID { get; set; } = string.Empty;
+        [JsonPropertyName("logIndex")]
+        public int LogIndex { get; set; }
+        [JsonPropertyName("body")]
+        public string Body { get; set; } = string.Empty;
+        [JsonPropertyName("integratedTime")]
+        public int IntegratedTime { get; set; }
+        [JsonPropertyName("attestation")]
+        public Attestation? Attestation { get; set; } = null;
+        [JsonPropertyName("verification")]
+        public Verification? Verification { get; set; } = null;	
 	}
 
 	public class Attestation
 	{
-		public string Data { get; set; } = string.Empty;
+        [JsonPropertyName("data")]
+        public string Data { get; set; } = string.Empty;
 	}
 
 	public class Verification
 	{
-		public InclusionProof InclusionProof { get; set; }	= new InclusionProof();
-		public string SignedEntryTimestamp { get; set; } = string.Empty;
+        [JsonPropertyName("inclusionProof")]
+        public InclusionProof InclusionProof { get; set; }	= new InclusionProof();
+        [JsonPropertyName("signedEntryTimestamp")]
+        public string SignedEntryTimestamp { get; set; } = string.Empty;
 
 	}
 
 	public class InclusionProof
 	{
-		public int LogIndex { get; set; }
-		public string RootHash { get; set; } = string.Empty;
-		public int TreeSize { get; set; }
-		public List<string> Hashes { get; set; } = new List<string>();
-		public string CheckPoint { get; set; } = string.Empty;
+        [JsonPropertyName("logIndex")]
+        public int LogIndex { get; set; }
+        [JsonPropertyName("rootHash")]
+        public string RootHash { get; set; } = string.Empty;
+        [JsonPropertyName("treeSize")]
+        public int TreeSize { get; set; }
+        [JsonPropertyName("hashes")]
+        public List<string> Hashes { get; set; } = new List<string>();
+        [JsonPropertyName("checkPoint")]
+        public string CheckPoint { get; set; } = string.Empty;
 	}
 }
