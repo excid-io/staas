78f780033384364d7c15107a5954fb0743a0cdb4
Nikos Fotiou <nikosft@gmail.com>
2025-12-09 23:24:07 +0200

    code restructured

--- Staas/Models/Staas/StaasException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Excid.Staas.Models
-{
-    public class StaasException: Exception
-    {
-        public StaasException() : base() { }
-        public StaasException(string message) : base(message) { }
-        public StaasException(string message, Exception inner) : base(message, inner) { }
-    }
-}
--- Staas/Security/IRegistrySigner.cs
+++
@@ -1,9 +0,0 @@
-﻿using Excid.Staas.Models;
-
-namespace Staas.Security
-{
-    public interface IRegistrySigner
-    {
-        Task<SignedItem> SignFileHash(string signer, byte[] hash, string comment);
-    }
-}
